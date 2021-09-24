9c3f4560c01336250abe5f5963b7709df1eaa11e
Dmytrenko-Roman <savitarreus@gmail.com>
2021-09-24 14:50:24 +0300

    ❄️ Lab7: half ❄️

--- Lab7/scripts/handler.js
+++ Lab7/scripts/handler.js
@@ -1,5 +1,36 @@
 'use strict';
 
+class EventHandler {
+  handleEvent(event) {
+    if (event.type === 'click') {
+      beetle_header.setAttribute("title", "Фольксваген Жук")
+    } else {
+      beetle_header.setAttribute("title", "Volkswagen Kever")
+    }
+  }
+}
+
 img.onclick = function loadMainPage() {
   location.assign("../Lab3/index.html");
 }
+
+function handler1() { 
+  alert("Volkswagen Beetle (вимова: укр. Фольксваґен бітл) — компактний автомобіль, який виробляється і продається компанією Volkswagen. Схожий на свого попередника Volkswagen New Beetle, однак дизайн автомобіля багато в чому ріднить його зі своїм далеким предком — оригінальним Volkswagen Käfer. Beetle більший, ніж New Beetle (на 149 мм довший) і базується на платформі A5 (PQ35) поряд з Volkswagen Jetta. Крім того, висота менша на 12 мм ніж у попередника, а ширина — на 87 мм більше. Обсяг багажника теж збільшився (310 л. Проти 209 л.). Друге покоління Beetle буде вироблятися на заводі Volkswagen в Пуебла, Мексика, Там само, де і Volkswagen Jetta. Кабріолет-версія з'явиться після купе в 2012 роцію.Слідом повинна вийти версія Beetle R."); 
+}
+
+function handler2() { 
+  alert('Невидима інформація.'); 
+}
+
+vw_beetle.onmouseover = function(event) {
+  const title = event.type + " на " + event.currentTarget;
+  vw_beetle.setAttribute("title", title);
+}
+
+vw_beetle.addEventListener('click', handler1);
+
+const event_handler = new EventHandler();
+
+beetle_header.addEventListener('click', event_handler);
+beetle_header.addEventListener('contextmenu', event_handler);
+beetle_header.removeEventListener('contextmenu', event_handler);
